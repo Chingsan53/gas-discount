5ab621f00e15b0e348519b08e4b573f1b4c6c470
Chingsan53 <cly53@csu.fullerton.edu>
2024-01-30 13:03:33 -0800

    update all names and gmail

--- src/App.js
+++ src/App.js
@@ -1,5 +1,158 @@
 import "./App.css";
 
+const numbers = [
+  {
+    name: "Brett Harding",
+    number: "",
+    gmail: "BrettHarding381@gmail.com",
+  },
+  {
+    name: "Rick Robertson",
+    number: "",
+    gmail: "RickRobertson766@gmail.com",
+  },
+  {
+    name: "Steven Adams",
+    number: "",
+    gmail: "StevenAdams964@gmail.com",
+  },
+  {
+    name: "Greg Stevenson",
+    number: "",
+    gmail: "GregStevenson454@gmail.com",
+  },
+  {
+    name: "Lara Howarth",
+    number: "",
+    gmail: "LaraHowarth011@gmail.com",
+  },
+  {
+    name: "Lewis Stapleton",
+    number: "",
+    gmail: "LewisStapleton991@gmail.com",
+  },
+  {
+    name: "Aaron Reed",
+    number: "",
+    gmail: "AaronReed960@gmail.com",
+  },
+  {
+    name: "Ann West",
+    number: "",
+    gmail: "AnnWest079@gmail.com",
+  },
+  {
+    name: "Jane Moffett",
+    number: "",
+    gmail: "JaneMoffett100@gmail.com",
+  },
+  {
+    name: "Gayle Holland",
+    number: "",
+    gmail: "GayleHolland593@gmail.com",
+  },
+  {
+    name: "Marcus Milstead",
+    number: "",
+    gmail: "MarcusMilstead758@gmail.com",
+  },
+  {
+    name: "Asa Heffernan",
+    number: "",
+    gmail: "AsaHeffernan843@gmail.com",
+  },
+  {
+    name: "Angela Travis",
+    number: "",
+    gmail: "AngelaTravis898@gmail.com",
+  },
+  {
+    name: "Cynthia Lachance",
+    number: "",
+    gmail: "CynthiaLachance667@gmail.com",
+  },
+  {
+    name: "Shantel Thompson",
+    number: "",
+    gmail: "ShantelThompson454@gmail.com",
+  },
+  {
+    name: "Bradley Miller",
+    number: "",
+    gmail: "BradleyMiller304@gmail.com",
+  },
+  {
+    name: "Steven Sterling",
+    number: "",
+    gmail: "StevenSterling931@gmail.com",
+  },
+  {
+    name: "Sherri Walter",
+    number: "",
+    gmail: "SherriWalter820@gmail.com",
+  },
+  {
+    name: "Nicholas Slone",
+    number: "",
+    gmail: "NicholasSlone030@gmail.com",
+  },
+  {
+    name: "Emanuel Lewis",
+    number: "",
+    gmail: "EmanuelLewis377@gmail.com",
+  },
+  {
+    name: "Thomas Roger",
+    number: "",
+    gmail: "ThomasRoger531@gmail.com",
+  },
+  {
+    name: "Barbara Duca",
+    number: "",
+    gmail: "BarbaraDuca944@gmail.com",
+  },
+  {
+    name: "William Baltz",
+    number: "",
+    gmail: "WilliamBaltz495@gmail.com",
+  },
+  {
+    name: "Gregory Robertson",
+    number: "",
+    gmail: "GregoryRobertson961@gmail.com",
+  },
+  {
+    name: "Marvin Ware",
+    number: "",
+    gmail: "MarvinWare509@gmail.com",
+  },
+  {
+    name: "Mary Saxton",
+    number: "",
+    gmail: "MarySaxton956@gmail.com",
+  },
+  {
+    name: "Richard Bronson",
+    number: "",
+    gmail: "RichardBronson806@gmail.com",
+  },
+  {
+    name: "Marcella Kerby",
+    number: "",
+    gmail: "MarcellaKerby990@gmail.com",
+  },
+  {
+    name: "Nyla Cyrus",
+    number: "",
+    gmail: "NylaCyrus132@gmail.com",
+  },
+  {
+    name: "Tammy Tong",
+    number: "",
+    gmail: "TammyTong197@gmail.com",
+  },
+];
+
 const App = () => {
   return (
     <div>
